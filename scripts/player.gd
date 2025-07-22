fe784bcdb4f367e868bdae0eac760822ccf8a4c4
Alberto Lorente <ask.digitaltoyz@gmail.com>
2025-07-23 00:13:57 +0200

    Añadida la escena del jugador y el script de movimiento básico

---
+++ scripts/player.gd
@@ -0,0 +1,28 @@
+extends CharacterBody2D
+
+@onready var spr = $AnimatedSprite
+
+const GRAVITY = 98
+const JFORCE = 1500
+const HSPEED = 750
+
+func _process(delta):
+	velocity.y += GRAVITY
+	
+	#Horizontal Movement
+	if Input.is_action_pressed("move_right"):
+		velocity.x = HSPEED
+	elif Input.is_action_pressed("move_left"):
+		velocity.x = -HSPEED
+	else:
+		velocity.x = 0
+	
+	#Flip
+	if velocity.x != 0:
+		spr.flip_h = velocity.x > 0
+	
+	#Vertical Movement
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = -JFORCE
+	
+	move_and_slide()
