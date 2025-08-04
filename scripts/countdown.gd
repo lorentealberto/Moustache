933087eacd21ba499d8738aeb1e6f58b19b4f490
Alberto Lorente <ask.digitaltoyz@gmail.com>
2025-08-04 19:50:57 +0200

    Animación para cuando el personaje salta o cae

--- scripts/countdown.gd
+++ scripts/countdown.gd
@@ -1,6 +1,8 @@
 extends Label
 
 @onready var anim: AnimationPlayer = $AnimationPlayer
+@onready var timer: Timer = $Timer
+
 var time_left: int = 3
 
 
@@ -12,6 +14,6 @@ func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	time_left -= 1
 	text = str(time_left)
 	
-	if time_left == 0:
+	if time_left <= 0:
 		Signals.game_started.emit()
 		queue_free()
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,39 +1,53 @@
 extends CharacterBody2D
 
 @onready var spr: AnimatedSprite2D = $AnimatedSprite
+@onready var anim: AnimationPlayer = $AnimationPlayer
 
 const GRAVITY: float = 98
 const JFORCE: float = 1500
 const HSPEED: float = 750
 
+var was_on_floor: bool = false
+var is_active: bool = false
 
-#To-Do
-# Comenzar animación una vez que el temporizador inicial haya terminado.
-# Hacer zoom antes de la animación de aparición.
-# La animación de aparición es una espiral.
-# El juego no comenzará hasta que no haya acabado la animación de aparición.
 
 func _process(delta: float) -> void:
-	velocity.y += GRAVITY
-	
-	#Horizontal Movement
-	if Input.is_action_pressed("move_right"):
-		velocity.x = HSPEED
-	elif Input.is_action_pressed("move_left"):
-		velocity.x = -HSPEED
-	else:
-		velocity.x = 0
-	
-	#Flip
-	if velocity.x != 0:
-		spr.flip_h = velocity.x > 0
-	
-	#Vertical Movement
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = -JFORCE
-	
-	move_and_slide()
+	if is_active:
+		velocity.y += GRAVITY
+		
+		#Horizontal Movement
+		if Input.is_action_pressed("move_right"):
+			velocity.x = HSPEED
+		elif Input.is_action_pressed("move_left"):
+			velocity.x = -HSPEED
+		else:
+			velocity.x = 0
+		
+		#Flip
+		if velocity.x != 0:
+			spr.flip_h = velocity.x > 0
+		
+		#Vertical Movement
+		if Input.is_action_just_pressed("jump") and is_on_floor():
+			velocity.y = -JFORCE
+			
+			anim.stop(true)
+			anim.play("jump")
+		
+		move_and_slide()
+		
+		if not was_on_floor and is_on_floor():
+			anim.stop(true)
+			anim.play("grounded")
+		
+		was_on_floor = is_on_floor()
 
 
 func _on_hit_box_area_entered(area: Area2D) -> void:
-	queue_free()
+	pass
+	#queue_free()
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "appear":
+		is_active = true
