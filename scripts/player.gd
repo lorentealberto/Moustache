8a858bd3c60a9f0671c43cf30302a4e8184877a8
Alberto Lorente <ask.digitaltoyz@gmail.com>
2025-08-04 20:05:07 +0200

    Movimiento suave del jugador

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,9 +6,12 @@ extends CharacterBody2D
 const GRAVITY: float = 98
 const JFORCE: float = 1500
 const HSPEED: float = 750
+const ACCEL: float = 20.0
+const DECCEL: float = 10.0
 
 var was_on_floor: bool = false
 var is_active: bool = false
+var direction: int = 0
 
 
 func _process(delta: float) -> void:
@@ -16,12 +19,18 @@ func _process(delta: float) -> void:
 		velocity.y += GRAVITY
 		
 		#Horizontal Movement
+		
 		if Input.is_action_pressed("move_right"):
-			velocity.x = HSPEED
+			direction = 1
 		elif Input.is_action_pressed("move_left"):
-			velocity.x = -HSPEED
+			direction = -1
 		else:
-			velocity.x = 0
+			direction = 0
+		
+		if direction != 0:
+			velocity.x = lerpf(velocity.x, direction * HSPEED, ACCEL * delta)
+		else:
+			velocity.x = lerpf(velocity.x, direction * HSPEED, DECCEL * delta)
 		
 		#Flip
 		if velocity.x != 0:
